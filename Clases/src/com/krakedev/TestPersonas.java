32c60abf375d88c1250c307c2e7c17c57002ffd1
Leonardo-Narvaez <leonardonarvaez12@gmail.com>
2025-11-19 23:01:05 -0500

    se crea la clase auto con los respectivos atributos y se crea ademas la clase TestAuto con main para crear los objetos y modificar sus atributos

---
+++ Clases/src/com/krakedev/TestPersonas.java
@@ -0,0 +1,10 @@
+package com.krakedev;
+
+public class TestPersonas {
+
+	public static void main(String[] args) {
+		Persona p;  // Declaracion de variable (la variable es como separa un espacio en la memoria para su uso)
+		p = new Persona(); //Instancia un objeto persona (esto va dentro de la variable, es como el valor que se le da) 
+	}
+
+}
